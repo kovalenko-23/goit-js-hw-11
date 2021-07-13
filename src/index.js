70c6764e09840c39bb72a0289be422c0cd14a9e4
Vitalii Kovalenko <vitaliikovalenko23@gmail.com>
2021-07-13 10:26:10 +0300

    counter added

--- src/index.js
+++ src/index.js
@@ -1,66 +1,121 @@
 import './sass/main.scss';
+import Swal from 'sweetalert2';
 
-const buttonStart = document.querySelector('#data-start');
-const buttonStop = document.querySelector('#data-stop');
-const outputDays = document.querySelector('[data-value="days"]');
-const outputHours = document.querySelector('[data-value="hours"]');
-const outputMins = document.querySelector('[data-value="mins"]');
-const outputSecs = document.querySelector('[data-value="secs"]');
-// const outputDays = document.querySelector('[data-label="days"]');
-// const outputHours = document.querySelector('[data-label="hours"]');
-// const outputMins = document.querySelector('[data-label="mins"]');
-// const outputSecs = document.querySelector('[data-label="secs"]');
+const dateSelector = document.querySelector('#date-selector');
+const startButton = document.querySelector('[data-start]');
 
+const outputDays = document.querySelector('[data-days]');
+const outputHours = document.querySelector('[data-hours]');
+const outputMins = document.querySelector('[data-minutes]');
+const outputSecs = document.querySelector('[data-seconds]');
 
+startButton.setAttribute('disabled', true);
 
 
-class timerCountdown {
-    constructor({ onTick }) {
-        this.onTick = onTick;
-    };
+dateSelector.addEventListener('change', event => {
+  startButton.removeAttribute('disabled');
+  const currentDate = Date.now();
+  const inputValue = event.target.value;
+  const targetDate = new Date(inputValue);
+  const targetTime = targetDate.getTime();
+  console.log(targetTime);
 
-    startCountdown() {
-        const currentDate = Date.now();
-        const targetDate = new Date('Jul 17, 2019');
-        const targetTime = targetDate.getTime();
-        const restTime = currentDate - targetTime;
+  if (currentDate > targetTime) {
+    startButton.setAttribute('disabled', true);
+    Swal.fire({
+      title: 'Error!',
+      text: 'Please choose a date in the future',
+      icon: 'error',
+      confirmButtonText: 'Return'
+    })
+  }
+
+});
+
+
+class TimerCountdown {
+  constructor({ onTick }) {
+    this.isActive = false;
+    this.intervalId = null;
+    this.onTick = onTick;
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+  }
+
+  startCountdown() {
+
+    if (this.isActive) {
+      return;
+    }
+    
+    this.isActive = true;
+
+    const currentDate = Date.now();
+    const targetDate = Date.parse(dateSelector.value);
+    console.log(targetDate);
+    const restTime = targetDate - currentDate;
         
-        setInterval(() => {
-            const currentTime = Date.now();
-            const deltaTime = currentTime - currentDate;
-            const timeCountdown = restTime - deltaTime;
-            const time = this.getTimeComponents(timeCountdown);
-            this.onTick(time);
-        }, 1000)
-        
-    }
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - currentDate;
+      const ms = restTime - deltaTime;
+      const time = this.convertMs(ms);
+      this.onTick(time);
+    }, 1000)
+  }
 
-    pad(value) {
-        return String(value).padStart(2, '0');
-    }
+  pad(value) {
+      return String(value).padStart(2, '0');
+  }
 
-    getTimeComponents(time) {
-        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+   convertMs(ms) {
+      // Number of milliseconds per unit of time
+      const second = 1000;
+      const minute = second * 60;
+      const hour = minute * 60;
+      const day = hour * 24;
 
-        return {days, hours, mins, secs}
-    }
+      // Remaining days
+      const days = this.pad(Math.floor(ms / day));
+      // Remaining hours
+      const hours = this.pad(Math.floor((ms % day) / hour));
+      // Remaining minutes
+      const minutes = this.pad(Math.floor(((ms % day) % hour) / minute));
+      // Remaining seconds
+      const seconds = this.pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+      return { days, hours, minutes, seconds };
+   } 
 }
 
-const newTimerCountdown = new timerCountdown({
+const newTimerCountdown = new TimerCountdown({
     onTick: updateOutput,
 });
 
-newTimerCountdown.startCountdown();
+startButton.addEventListener('click', event => {
+  newTimerCountdown.startCountdown();
+});
 
-function updateOutput({ days, hours, mins, secs }) {
+dateSelector.addEventListener('click', event => {
+  newTimerCountdown.stop();
+});
+
+function updateOutput({ days, hours, minutes, seconds }) {
     outputDays.textContent = `${days}`;
     outputHours.textContent = `${hours}`;
-    outputMins.textContent = `${mins}`;
-    outputSecs.textContent = `${secs}`;
+    outputMins.textContent = `${minutes}`;
+    outputSecs.textContent = `${seconds}`;
 }
+
+
+
+
+
+
+
 
 
        
